9dbb655c9f00b07e39a78d20b8715d0d67c956ac
AAboEida <aboeidaahmed563@gmail.com>
2025-12-30 15:32:04 +0200

    complete changes and templates

--- my_project/blog/admin.py
+++ my_project/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','author','publish','created','updated']
+    list_filter = ['publish','created']
+    search_fields = ['title']
+    date_hierarchy = 'publish'
+    raw_id_fields=['author']
+    prepopulated_fields = {'slug':('title',)}
+    ordering = ['publish','status']
+
+    
